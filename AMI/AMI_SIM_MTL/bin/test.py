1ca2c24c47e1b5116d44c7538ab021d0e688f930
njcuk9999 <neil.james.cook@gmail.com>
2020-05-27 19:10:49 -0400

    AMI_SMI_MTL.bin.test.py - example code with basic constants

--- AMI/AMI_SIM_MTL/bin/test.py
+++ AMI/AMI_SIM_MTL/bin/test.py
@@ -9,15 +9,48 @@ Created on 2020-05-21
 
 @author: cook
 """
+from ami_sim_mtl.core.instrument import constants
+from ami_sim_mtl.core.core import param_functions
 
 # =============================================================================
 # Define variables
 # =============================================================================
+# set name
+__NAME__ = 'bin/test.py'
+__DESCRIPTION__ = 'test script for AMI_SIM_MTL'
+# get default constants
+consts = constants.Consts
+# copy for update
+lconsts = consts.copy(__NAME__)
+# set very basic constants
+__VERSION__ = lconsts.constants['PACKAGE_VERSION'].value
+__DATE__ = lconsts.constants['PACKAGE_VERSION_DATE'].value
+
+# add code specific arguments
+
+# Define the scene fits file
+lconsts.add_argument('SCENE', value=None, dtype=str,
+                     source=__NAME__, user=True, argument=True,
+                     group=__NAME__, description='Define the scene fits file',
+                     command=['--scene'])
 
 
 # =============================================================================
 # Define functions
 # =============================================================================
+def main(**kwargs):
+    # get params (run time + config file + constants file)
+    params = param_functions.setup(lconsts, kwargs,
+                                   description=__DESCRIPTION__)
+    # run the __main__ to return products
+    if not params['GENERATE_CONFIG_FILE']:
+        __main__(params)
+
+
+def __main__(params):
+    # main code here
+    for param in params:
+        params.info(param)
 
 
 # =============================================================================
@@ -25,7 +58,7 @@ Created on 2020-05-21
 # =============================================================================
 if __name__ == "__main__":
     # run main code
-    print('Hello World')
+    ll = main()
 
 # =============================================================================
 # End of code
