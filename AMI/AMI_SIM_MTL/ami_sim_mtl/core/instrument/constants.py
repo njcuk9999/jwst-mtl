8da5b66c2a6171de52d4f5fb21fecb733331d928
njcuk9999 <neil.james.cook@gmail.com>
2020-05-27 19:10:30 -0400

    AMI_SMI_MTL.core.instrument.constants.py - remove use of units

--- AMI/AMI_SIM_MTL/ami_sim_mtl/core/instrument/constants.py
+++ AMI/AMI_SIM_MTL/ami_sim_mtl/core/instrument/constants.py
@@ -9,8 +9,6 @@ Created on 2020-05-21
 
 @author: cook
 """
-from astropy import units as uu
-
 from ami_sim_mtl.core.core import constant_functions
 
 # set very basic constants
@@ -49,23 +47,34 @@ group = 'General'
 
 # Define the user config file
 Consts.add('USER_CONFIG_FILE', value=None, dtype='path',
-           source=__NAME__, user=True, argument=True, group=group,
+           source=__NAME__, user=False, argument=True, group=group,
            description='Define the user config file', command='--config')
 
 # Define the package name
 Consts.add('PACKAGE_NAME', value='ami_sim_mtl', dtype=str,
-           source=__NAME__, user=True, group=group,
+           source=__NAME__, user=False, group=group,
            description='Define the package name')
 
 # Define the package version
 Consts.add('PACKAGE_VERSION', value=__VERSION__, dtype=str,
-           source=__NAME__, user=True, group=group,
+           source=__NAME__, user=False, group=group,
            description='Define the package version')
 
 # Define the version date
 Consts.add('PACKAGE_VERSION_DATE', value=__DATE__, dtype=str,
-           source=__NAME__, user=True, group=group,
+           source=__NAME__, user=False, group=group,
            description='Define the package version date')
+
+# Define whether we want to generate a config file
+Consts.add('GENERATE_CONFIG_FILE', value=False, dtype=bool,
+           source=__NAME__, user=False, argument=True, group=group,
+           description='Define whether we want to generate a config file',
+           command='--getconfig')
+
+# Define an output directory
+Consts.add('OUTDIR', value=None, dtype=str, source=__NAME__, user=True,
+           argument=True, group=group, description='Define an output directory',
+           command='--out')
 
 
 # =============================================================================
@@ -79,7 +88,7 @@ Consts.add('ADD_JITTER', value=True, dtype=bool,
            command='--add_jitter')
 
 # Define whether to add the jitter
-Consts.add('JITTER_RMS', value=7.0, dtype=float, units=uu.mas,
+Consts.add('JITTER_RMS', value=7.0, dtype=float,
            source=__NAME__, user=True, argument=True, group=group,
-           description='Define the jitter rms level',
+           description='Define the jitter rms level [mas]',
            command='--jitter_rms')
