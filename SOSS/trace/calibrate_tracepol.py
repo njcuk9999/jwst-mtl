calibrate_tracepol.py and tracepol.py - Fixed a bug in calibrate_tracepol that made the fit fail. Updated the output figure and updated tracepol with the best fit parameters for rotation and offset (usinng order 1 and 2 CV3 measurements in the fit).

SOSS/trace/calibrate_tracepol.py
@@ -3,6 +3,7 @@ from astropy.io import fits
 import matplotlib.pylab as plt
 from scipy import interpolate
 from scipy.optimize import least_squares
+from scipy.ndimage import map_coordinates
 
 import sys
 sys.path.insert(0, '/genesis/jwst/github/jwst-mtl/')
@@ -43,9 +44,9 @@ def get_CV3_tracepars(order=1):
         # (measured in the native pixel coordinates).
         spatpix = 256 - np.array([60,161,161])
         specpix = 2048 - np.array([395,1308,1823])
-        w = np.array([0.6412, 1.06262,1.30838])   # microns
+        w = np.array([0.6412, 1.06262, 1.30838])   # microns
         # Fit the specpix vs wavelength
-        param_spec = np.polyfit(w, specpix, 1)
+        param_spec = np.polyfit(w, specpix, 2)
         if False:
             wfit = np.linspace(0.6,1.4,200)
             xfit = np.polyval(param_spec, wfit)
@@ -162,6 +163,9 @@ def calibrate_tracepol():
     badpix = np.zeros_like(bad, dtype='bool')
     badpix[~np.isfinite(bad)] = True
 
+    # Interpolate bad pixels
+    im = interpolate_badpixels(im, badpix)
+
     # Measure the trace centroid position for the deep stack image.
     centroids = get_soss_centroids(im, subarray='SUBSTRIP256', apex_order1=None,
                                     mask=badpix, verbose=False)
@@ -238,8 +242,7 @@ def calibrate_tracepol():
     # Call tracepol, disabling the default rotation, back to original
     # Optics Model. x/y_mod_N are realization of the model at a few
     # wavelengths.
-    param = tp.get_tracepars(filename=optmodel, disable_rotation=False)
-
+    param = tp.get_tracepars(filename=optmodel, disable_rotation=True)
     x_mod_o1, y_mod_o1, mask_mod_o1 = tp.wavelength_to_pix(wavelength_o1,
                                                param, m=1,
                                                frame='dms',
@@ -268,22 +271,24 @@ def calibrate_tracepol():
         plt.show()
 
         plt.figure(figsize=(10,10))
-        plt.plot(wavelength_o1, x_obs_o1)
-        plt.plot(wavelength_o1, x_mod_o1)
+        plt.scatter(wavelength_o1, x_obs_o1)
+        plt.scatter(wavelength_o1, x_mod_o1)
         plt.show()
 
     # What orders should be used for fitting for rotation?
     if True:
         # Package the Orders 1 and 2 model points and observation points
+        print('Fitting orders 1 and 2 in obtaining best rotation')
         x_mod = np.concatenate((x_mod_o1, x_mod_o2), axis=None)
-        y_mod = np.concatenate((y_mod_o2, y_mod_o2), axis=None)
+        y_mod = np.concatenate((y_mod_o1, y_mod_o2), axis=None)
         x_obs = np.concatenate((x_obs_o1, x_obs_o2), axis=None)
         y_obs = np.concatenate((y_obs_o1, y_obs_o2), axis=None)
         xy_obs = np.array([x_obs, y_obs])
     else:
         # Package the Orders 1 ONLY model points and observation points
+        print('Fitting only first order in obtaining best rotation')
         x_mod = np.copy(x_mod_o1)
-        y_mod = np.copy(y_mod_o2)
+        y_mod = np.copy(y_mod_o1)
         x_obs = np.copy(x_obs_o1)
         y_obs = np.copy(y_obs_o1)
         xy_obs = np.array([x_obs, y_obs])
@@ -316,35 +321,32 @@ def calibrate_tracepol():
     #bounds=([-np.inf,-np.inf,-np.inf,-0.0001,-0.0001],[np.inf,np.inf,np.inf,0,0])) - no need Do the maths
     param_bestfit = res2.x
 
-    # OVERRIDE !!!
-    # Best fit obtained with prior version of this script (cv3_calibration)
-    param_bestfit = [-1.3868425075, 1577.9020186702, -1109.1909267381]
-    res2.x = param_bestfit
-    # TODO: Find the bug that makes the fit be wrong.
+    print('Best fit parameters:',param_bestfit)
 
-    print(res2)
-    print('cost = {:}'.format(res2.cost))
-    print('Best fit parameters (in DMS coordinates):')
-    print('theta = {:15.10f}'.format(res2.x[0]))
-    print('origin_x = {:15.10f}'.format(res2.x[1]))
-    print('origin_y = {:15.10f}'.format(res2.x[2]))
-    #print('offset_x = {:15.10f}'.format(res2.x[3]))
-    #print('offset_y = {:15.10f}'.format(res2.x[4]))
-    print()
-    print('Best fit parameters (in native (ds9) coordinates):')
-    print('theta = {:15.10f}'.format(-res2.x[0]))
-    print('origin_x = {:15.10f}'.format(256-res2.x[2]))
-    print('origin_y = {:15.10f}'.format(2048-res2.x[1]))
-    #print('offset_x = {:15.10f}'.format(-res2.x[4]))
-    #print('offset_y = {:15.10f}'.format(-res2.x[3]))
-    print()
-    print('Once converted to native (aka ds9) pixel coordinates used by tracepol.py,')
-    print('this becomes:')
-    print('get_tracepars(filename=None, origin=np.array([{:}, {:}]),'.format(256-res2.x[2], 2048-res2.x[1]))
-    print('              angle={:},'.format(-res2.x[0]))
-    print('              disable_rotation=False):')
+    if True:
+        print('cost = {:}'.format(res2.cost))
+        print('Best fit parameters (in DMS coordinates):')
+        print('theta = {:15.10f}'.format(res2.x[0]))
+        print('origin_x = {:15.10f}'.format(res2.x[1]))
+        print('origin_y = {:15.10f}'.format(res2.x[2]))
+        #print('offset_x = {:15.10f}'.format(res2.x[3]))
+        #print('offset_y = {:15.10f}'.format(res2.x[4]))
+        print()
+        print('Best fit parameters (in native (ds9) coordinates):')
+        print('theta = {:15.10f}'.format(-res2.x[0]))
+        print('origin_x = {:15.10f}'.format(256-res2.x[2]))
+        print('origin_y = {:15.10f}'.format(2048-res2.x[1]))
+        #print('offset_x = {:15.10f}'.format(-res2.x[4]))
+        #print('offset_y = {:15.10f}'.format(-res2.x[3]))
+        print()
+        print('Once converted to native (aka ds9) pixel coordinates used by tracepol.py,')
+        print('this becomes:')
+        print('get_tracepars(filename=None, origin=np.array([{:}, {:}]),'.format(256-res2.x[2], 2048-res2.x[1]))
+        print('              angle={:},'.format(-res2.x[0]))
+        print('              disable_rotation=False):')
 
     # Check that the rotated points overplot the observations
+    # Convert from dms to ds9 coordinates
     x_fit_o1, y_fit_o1 = apply_calibration(param_bestfit, x_mod_o1, y_mod_o1)
     x_fit_o2, y_fit_o2 = apply_calibration(param_bestfit, x_mod_o2, y_mod_o2)
     x_fit_o3, y_fit_o3 = apply_calibration(param_bestfit, x_mod_o3, y_mod_o3)
@@ -357,33 +359,134 @@ def calibrate_tracepol():
     if please_check:
 
         # Figure to show the positions for all 3 orders
-        plt.figure(figsize=(10, 10))
-        plt.xlim((0, 2048))
-        plt.ylim((0, 256))
-        plt.imshow(np.log10(im), vmin=0.7, vmax=3, origin='lower', aspect='auto')
+        fig = plt.figure(figsize=(8, 8), constrained_layout=True)
+        layout = """
+            AAAAB
+            AAAAB
+            AAAAB
+            AAAAB
+            CCCC.
+            """
+        frame = fig.subplot_mosaic(layout)
 
-        plt.plot(x_o1, y_o1, color='orange', label='Order 1 - CV3')
-        plt.plot(x_mod_o1, y_mod_o1, linestyle='dashed', color='orange', label='Order 1 - Model')
-        plt.scatter(x_fit_o1, y_fit_o1, color='orange', label='Order 1 - Model rotated')
+        # Colors for the 3 orders in the figure
+        color_o1 = 'navy'
+        color_o2 = 'orange'
+        color_o3 = 'red'
+
+        # First recalculate model positions for all observed positions
+        param = tp.get_tracepars(filename=optmodel, disable_rotation=False)
+        x_mod_o1, y_mod_o1, mask_mod_o1 = tp.wavelength_to_pix(w_o1,
+                                                               param, m=1,
+                                                               frame='dms',
+                                                               subarray='SUBSTRIP256',
+                                                               oversample=1)
+        x_mod_o2, y_mod_o2, mask_mod_o2 = tp.wavelength_to_pix(w_o2,
+                                                               param, m=2,
+                                                               frame='dms',
+                                                               subarray='SUBSTRIP256',
+                                                               oversample=1)
+        x_mod_o3, y_mod_o3, mask_mod_o3 = tp.wavelength_to_pix(w_o3,
+                                                               param, m=3,
+                                                               frame='dms',
+                                                               subarray='SUBSTRIP256',
+                                                               oversample=1)
+
+        # Determine the wavelength boundaries for nice display purposes
+        wmin1, wmax1 = tp.subarray_wavelength_bounds(param, subarray='SUBSTRIP256', m=1,
+                                       specpix_offset=0, spatpix_offset=0)
+        wmin2, wmax2 = tp.subarray_wavelength_bounds(param, subarray='SUBSTRIP256', m=2,
+                                       specpix_offset=0, spatpix_offset=0)
+        wmin3, wmax3 = tp.subarray_wavelength_bounds(param, subarray='SUBSTRIP256', m=3,
+                                       specpix_offset=0, spatpix_offset=0)
+        indo1 = (w_o1 >= wmin1) & (w_o1 <= wmax1)
+        indo2 = (w_o2 >= wmin2) & (w_o2 <= wmax2)
+        indo3 = (w_o3 >= wmin3) & (w_o3 <= wmax3)
+
+        frame['A'].set_xlim((0,2048))
+        frame['A'].set_ylim((0,256))
+        frame['A'].imshow(np.log10(im), vmin=0.7, vmax=3, origin='lower', aspect='auto')
+
+        frame['A'].plot(x_o1, y_o1, color=color_o1, label='Order 1 - CV3')
+        frame['A'].plot(x_mod_o1, y_mod_o1, linestyle='dashed', color=color_o1, label='Order 1 - Model')
+        #frame['A'].scatter(x_fit_o1, y_fit_o1, color=color_o1, label='Order 1 - Model rotated')
 
         if x_o2 is not None:
-            plt.plot(x_o2, y_o2, color='black', label='Order 2 - CV3')
-            plt.plot(x_mod_o2, y_mod_o2, linestyle='dashed', color='black', label='Order 2 - Model')
-            plt.scatter(x_fit_o2, y_fit_o2, color='black', label='Order 2 - Model rotated')
+            frame['A'].plot(x_o2, y_o2, color=color_o2, label='Order 2 - CV3')
+            frame['A'].plot(x_mod_o2, y_mod_o2, linestyle='dashed', color=color_o2, label='Order 2 - Model')
+            #frame['A'].scatter(x_fit_o2, y_fit_o2, color=color_o2, label='Order 2 - Model rotated')
 
         if x_o3 is not None:
-            plt.plot(x_o3, y_o3, color='red', label='Order 3 -CV3')
-            plt.plot(x_mod_o3, y_mod_o3, linestyle='dashed', color='red', label='Order 3 - Model')
-            plt.scatter(x_fit_o3, y_fit_o3, color='red', label='Order 3 - Model rotated')
+            frame['A'].plot(x_o3[indo3], y_o3[indo3], color=color_o3, label='Order 3 - CV3')
+            frame['A'].plot(x_mod_o3[indo3], y_mod_o3[indo3], linestyle='dashed', color=color_o3, label='Order 3 - Model')
+            #frame['A'].scatter(x_fit_o3, y_fit_o3, color=color_o3, label='Order 3 - Model rotated')
 
-        plt.legend()
+        frame['A'].xaxis.set_ticks_position('top')
+        frame['A'].set_xlabel('Detector Column Position')
+        frame['A'].yaxis.set_label_position('right')
+        frame['A'].set_ylabel('Detector Row Position (Stretched)')
+        frame['A'].legend()
+
+
+        # Position residuals on the x axis
+
+        # residuals are
+        resi_x_o1 = x_o1 - x_mod_o1
+        resi_x_o2 = x_o2 - x_mod_o2
+        resi_x_o3 = x_o3 - x_mod_o3
+        resi_y_o1 = y_o1 - y_mod_o1
+        resi_y_o2 = y_o2 - y_mod_o2
+        resi_y_o3 = y_o3 - y_mod_o3
+
+        frame['C'].plot([0,2048],[0,0],linestyle='dashed',color='black')
+        frame['C'].plot(x_o1[indo1], resi_x_o1[indo1], color=color_o1)
+        frame['C'].plot(x_o2[indo2], resi_x_o2[indo2], color=color_o2)
+        frame['C'].plot(x_o3[indo3], resi_x_o3[indo3], color=color_o3)
+        frame['C'].set_xlim((0, 2048))
+        frame['C'].set_ylim((-11,11))
+        frame['C'].yaxis.set_ticks_position('left')
+        frame['C'].yaxis.set_label_position('right')
+        frame['C'].set_ylabel('X Pixel Deviation')
+
+        frame['B'].plot([0,0],[0,256],linestyle='dashed',color='black')
+        frame['B'].plot(resi_y_o1[indo1], y_o1[indo1], color=color_o1)
+        frame['B'].plot(resi_y_o2[indo2], y_o2[indo2], color=color_o2)
+        frame['B'].plot(resi_y_o3[indo3], y_o3[indo3], color=color_o3)
+        frame['B'].set_xlim((-5, 5))
+        frame['B'].set_ylim((0, 256))
+        frame['B'].yaxis.set_ticks_position('right')
+        frame['B'].xaxis.set_ticks_position('top')
+        frame['B'].set_xlabel('Y Pixel Deviation')
+
+        plt.savefig('/genesis/jwst/userland-soss/loic_review/traces_position_CV3_vs_Optics.png')
         plt.show()
 
-    # TODO: Add a measurement of the quality of the fit. i.e. the pixel
-    # position rms deviations between rotated optics model and
-    # observations.
 
 
+def interpolate_badpixels(image, badpix):
+    '''
+    Interpolate the bad pixels
+    '''
+
+    # Work on a copy of the image
+    dimy, dimx = np.shape(image)
+    image_corr = np.copy(image)
+
+    # Indices of the bad pixels and any NaN pixel
+    indy, indx = np.where((badpix == True) | (np.isfinite(image) == False))
+
+    # Determine the coordinates of the 8 pixels around the bad pixel
+    x0, x1, y0, y1 = indx-1, indx+1, indy-1, indy+1
+    # Keep  those within the image boundaries
+    indx0, indx1, indy0, indy1 = x0 < 0, x1 > dimx-1, y0 < 0, y1 > dimy-1
+    x0[indx0], x1[indx1], y0[indy0], y1[indy1] = 0, dimx-1, 0, dimy-1
+
+    # Interpolate pixels one by one
+    for i in range(np.size(indx)):
+        badval = np.nanmean(image_corr[y0[i]:y1[i],x0[i]:x1[i]])
+        image_corr[indy[i],indx[i]] = badval
+
+    return image_corr
 
 #run
 calibrate_tracepol()

SOSS/trace/tracepol.py
@@ -14,7 +14,7 @@ from astropy.io import ascii
 # SUBSTRIP96 keeps columns 150:245 (0 based) in the nat frame.
 
 
-def apply_rotation(coords, origin=np.array([1365.1909267381,470.0979813298]), angle=1.3868425075):
+def apply_rotation(coords, origin=np.array([1419.8897384173,472.9340739229]), angle=1.3824300138):
 
     """
     Rotate a point counterclockwise by a given angle around a given origin.
@@ -107,8 +107,9 @@ def trace_polynomial(trace, m=1, maxorder=15):
     return pars
 
 
-def get_tracepars(filename=None, origin=np.array([1365.1909267381, 470.0979813298]), angle=1.3868425075,
-                  disable_rotation=False):
+#def get_tracepars(filename=None, origin=np.array([1365.1909267381, 470.0979813298]), angle=1.3868425075,
+#                  disable_rotation=False):
+def get_tracepars(filename=None, origin=None, angle=None, disable_rotation=False):
 
     """Read a file containing the trace profile and generate
     polynomial parameters for each order.
@@ -140,9 +141,17 @@ def get_tracepars(filename=None, origin=np.array([1365.1909267381, 470.097981329
     trace['xpos'] -= 0.5  # Set the origin at the center of the lower-left pixel.
     trace['ypos'] -= 0.5  # Set the origin at the center of the lower-left pixel.
 
-    # Apply rotation around point.
+    # Apply rotation around point (by default)
     if disable_rotation == False:
-        trace['xpos'], trace['ypos'] = apply_rotation((trace['xpos'], trace['ypos']), origin=origin, angle=angle)
+        print('get_tracepars - Apply rotation to Optics model.')
+        if (origin is not None) & (angle is not None):
+            trace['xpos'], trace['ypos'] = apply_rotation((trace['xpos'], trace['ypos']), origin=origin, angle=angle)
+        elif angle is not None:
+            trace['xpos'], trace['ypos'] = apply_rotation((trace['xpos'], trace['ypos']), angle=angle)
+        elif origin is not None:
+            trace['xpos'], trace['ypos'] = apply_rotation((trace['xpos'], trace['ypos']), origin=origin)
+        else:
+            trace['xpos'], trace['ypos'] = apply_rotation((trace['xpos'], trace['ypos']))
 
     # Compute polynomial parameters for different orders.
     tracepars = dict()