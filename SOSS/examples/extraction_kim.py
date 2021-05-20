f5af232af1214b535d7869c09742abcf432a83c7
kmorel <kim.morel@umontreal.ca>
2021-05-20 16:53:26 -0400

    Continuation avec flux extraction_kim.py

--- SOSS/examples/extraction_kim.py
+++ SOSS/examples/extraction_kim.py
@@ -6,6 +6,7 @@ import itsosspipeline as soss
 import sys
 import os
 import specgen.spgen as spgen
+import scipy.constants as sc_cst
 
 WORKING_DIR = '/home/kmorel/ongenesis/jwst-user-soss/'
 
@@ -39,11 +40,13 @@ dq = a[3].data  #Data quality
 dq = dq[0,:,:]
 
 #VALEUR IMPAIRES DE DQ = PIXELS À NE PAS UTILISER
+i = np.where(dq %2 != 0)
+im[i[0],i[1]] = 0
 
 ynew = np.interp(x, xnew, y)    #Interpolation of y at integer values of x
 ynew = np.around(ynew)
 
-spec = np.zeros_like(x)
+spec = np.zeros_like(x)   #Extracted spectrum
 for x_i in x:
     x_i = int(x_i)
     i = int(ynew[x_i])
@@ -54,14 +57,32 @@ for x_i in x:
     #im[:,x_i] -= residu_light
     spec[x_i] = np.sum(im[i-15:i+15,x_i])
 
+g = 1.6  #e⁻/adu
+energy = sc_cst.Planck*sc_cst.speed_of_light/(w*10**(-6))
+area = 25  #m²
 
-plt.figure()
+dw = np.zeros_like(w)
+for i in range(len(w)):
+    if i == 0:
+        dw[i] = w[0]-w[1]
+    else:
+        dw[i] = w[i-1]-w[i]
+
+f_lambda = spec*g*energy/area/dw  #Flux
+
+plt.figure(1)
 plt.imshow(im, vmin=0, vmax=1000, origin="lower")
 plt.plot(x, y, color="red")
 plt.show()
 
 
-plt.figure()
-plt.plot(w, spec, color="HotPink")
-#plt.plot(starmodel_angstrom, starmodel_flambda,color='Aqua')
+plt.figure(2)
+#plt.plot(w, spec, color="HotPink")
+plt.plot(w, f_lambda, color="HotPink")
+plt.xlabel("Wavelength [microns]"), plt.ylabel("Flux [W/m²/micron]")
+plt.show()
+
+plt.figure(3)
+plt.plot(starmodel_angstrom, starmodel_flambda,color='Aqua')
+plt.xlabel("Wavelength [angstroms]"), plt.ylabel("Flux [W/m²/angstrom]")
 plt.show()
