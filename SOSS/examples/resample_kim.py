0327e6037496f7b1238ac533190a60022482df0a
kmorel <kim.morel@umontreal.ca>
2021-06-09 16:07:34 -0400

    Convolution resample_kim.py

--- SOSS/examples/resample_kim.py
+++ SOSS/examples/resample_kim.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 def resample_models(dw,starmodel_wv, starmodel_flux, ld_coeff, planetmodel_wv, planetmodel_rprs, pars, tracePars):
     """Resamples star and planet model onto common grid.
 
@@ -27,13 +26,107 @@ def resample_models(dw,starmodel_wv, starmodel_flux, ld_coeff, planetmodel_wv, p
         bin_planetmodel_wv - binned planet wavelength array (should be same size as bin_starmodel_wv)
         bin_planetmodel_rprs - binned Rp/R* array
     """
-    bin_starmodel_wv = starmodel_wv
-    bin_starmodel_flux = starmodel_flux
+
+    """
+    # To get constant dispersion
+    star_grid = np.copy(starmodel_wv)
+    dstar_grid = np.zeros_like(starmodel_wv)
+    dstar_grid[1:] = np.abs(star_grid[1:] - star_grid[0:-1])
+    dstar_grid[0] = dstar_grid[1]
+    new_dstar = np.min(dstar_grid)  # Plus petit pas
+    new_star_grid = np.arange(np.min(starmodel_wv),np.max(starmodel_wv)+new_dstar, new_dstar)  #New array de wv
+    new_star_flux = np.interp(new_star_grid, star_grid, starmodel_flux)  #New flux interpolated with new_dwv
+    # The points of starmodel_flux are now equally spaced in order to do the convolution.
+    """
+
+    # Resample on the same grid as the planet model grid
+    x_grid = np.copy(planetmodel_wv)
+    dx_grid = np.zeros_like(planetmodel_wv)
+    dx_grid[1:] = np.abs(x_grid[1:] - x_grid[0:-1])
+    dx_grid[0] = dx_grid[1]
+
+    # Resample star model and limb darkening matrix. But leave the planet model unchanged.
+    bin_starmodel_flux = bin_array_conv(starmodel_wv, starmodel_flux, x_grid, dx_grid)
+    bin_starmodel_wv = np.copy(planetmodel_wv)
+    bin_planetmodel_wv = np.copy(planetmodel_wv)
+    bin_planetmodel_rprs = np.copy(planetmodel_rprs)
+    bin_ld_coeff = bin_limb_darkening(starmodel_wv, ld_coeff, x_grid, dx_grid)
+
     plt.figure()
-    plt.plot(bin_starmodel_wv[800000:804000], bin_starmodel_flux[800000:804000])
+    plt.plot(starmodel_wv, starmodel_flux, color='b')
+    plt.plot(bin_starmodel_wv, bin_starmodel_flux, ls='--', color='r')
     plt.show()
-    d_wv = np.empty(shape=(len(starmodel_wv)-1))
-    for i in range(len(starmodel_wv)-1):
-        d_wv[i] = starmodel_wv[i+1] - starmodel_wv[i]
-    print(d_wv)
-    return bin_starmodel_wv, bin_starmodel_flux, bin_ld_coeff,bin_planetmodel_wv, bin_planetmodel_rprs
+    return bin_starmodel_wv, bin_starmodel_flux, bin_ld_coeff, bin_planetmodel_wv, bin_planetmodel_rprs
+
+def bin_array_conv(starmodel_wv, starmodel_flux, planetmodel_wv, dx_grid):
+    # For R constant (both for the planet and the star)
+    R_s = np.mean(starmodel_wv[:-1] / np.diff(starmodel_wv))
+    R_p = np.mean(planetmodel_wv[:-1] / np.diff(planetmodel_wv))
+    fwhm = R_s / R_p    # FWHM
+    sig = fwhm / np.sqrt(8*np.log(2))  # Convert FWHM to sigma for gaussian
+    length = int(5 * fwhm)   # Length of gaussian array
+    if length%2 == 0:
+        length += 1
+    elif length%2 == 1:
+        None
+
+    x_gauss = np.mgrid[-length//2 : length//2+1]   # x grid for gaussian
+    gauss = np.exp(-x_gauss ** 2 / 2 / sig ** 2)   # Gaussian
+    gauss = gauss / np.sum(gauss)   # Normalization of gaussian
+
+    conv_star_flux = np.convolve(starmodel_flux, gauss, mode='same')
+
+    return np.interp(x_grid, starmodel_wv, conv_star_flux)
+
+def bin_limb_darkening(x, ld_coeff, x_grid, dx_grid):
+    '''
+    Bin down each parameter of the limb darkening coefficient
+    on the new x_grid using the function bin_array()
+    :param x:
+    :param ld_coeff:
+    :param x_grid:
+    :param dx_grid:
+    :return:
+    '''
+    # Shape of the ld_coeff input: (200001,4)
+    # Shape of the output bin_ld_coeff: (n,4)
+    bin_ld_coeff = np.zeros((len(x_grid), 4))
+    for i in range(4):
+        param_i = ld_coeff[:,i]
+        bin_param_i = bin_array(x, param_i, x_grid, dx_grid)
+        bin_ld_coeff[:,i] = np.copy(bin_param_i)
+    return bin_ld_coeff
+
+def bin_array(x, fx, x_grid, dx_grid):
+    '''
+    :param x:
+    :param fx: represents the array values at each x sample
+    :param x_grid: represents the sample center of the binned array
+    :param dx_grid: represents the sample width of the bined array
+    :return:
+    '''
+    # Arrays of the starting and ending x_grid values for each grid sample
+    xgrid_left = x_grid - dx_grid/2
+    xgrid_right = x_grid + dx_grid/2
+    # Arrays of resampled fx at the position of the left and right x_grid
+    fx_resampleft = np.interp(xgrid_left, x, fx)
+    fx_resampright = np.interp(xgrid_right, x, fx)
+    # Initialize the binned flux output array
+    fx_grid = np.zeros_like(x_grid)
+    # Sum up each original bin (fx * dx)
+    grid_nsample = len(x_grid)
+    for i in range(grid_nsample):
+        print(i, grid_nsample)
+        sum = 0.0
+        # Original sample points fully included in new grid current bin
+        ind = np.where((x >= xgrid_left[i]) & (x <= xgrid_right[i]))[0]
+        # Integral for those full original samples
+        for j in range(len(ind)-1):
+            sum = sum + (fx[ind[j+1]] + fx[ind[j]])/2 * (x[ind[j+1]] - x[ind[j]])
+        # Partial sample on the left
+        sum = sum + (fx[ind[0]]+fx_resampleft[i])/2 * (x[ind[0]] - xgrid_left[i])
+        # Partial sample on the right
+        sum = sum + (fx[ind[-1]]+fx_resampright[i])/2 * (xgrid_right[i] - x[ind[-1]])
+        #To get fx_grid, need to divide sum by the width of that binned grid
+        fx_grid[i] = sum / dx_grid[i]
+    return fx_grid
