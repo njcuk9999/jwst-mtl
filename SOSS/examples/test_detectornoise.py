b5f328f4d6cbc57530fd3985d65b5f99914efff0
Loic for the Team <albert@astro.umontreal.ca>
2021-10-08 12:52:45 -0400

    timeseries.py - use dtype np.int16 rather than np.uint16 in writing the noisy file to disk. Otherwise, with readout noise alone, pixel values would wrap around 65535 instead of being negative values.

---
+++ SOSS/examples/test_detectornoise.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+import detector.detector as detector
+
+from astropy.io import fits
+
+files = '/genesis/jwst/userland-soss/loic_review/IDTSOSS_clear.fits'
+a = detector.add_noise(files, '/genesis/jwst/jwst-ref-soss/noise_files/',
+                       outputfilename='/genesis/jwst/userland-soss/loic_review/toto.fits',
+                       readout=True,
+                       zodibackg=False,
+                       photon=False,
+                       superbias=False,
+                       flatfield=False,
+                       nonlinearity=False,
+                       oneoverf=False,
+                       darkcurrent=False)
+
+#fits.writeto(a, '/genesis/jwst/userland-soss/loic_review/toto.fits', overwrite=True)
+
+
+
+
+
+
+
+
+
